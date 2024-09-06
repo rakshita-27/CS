3f2e6bdd7da47dd4e4125a284e19f8c47125c56d
rakshita-27 <130498747+rakshita-27@users.noreply.github.com>
2024-09-06 06:58:13 +0000

    Update CALCULATOR.py

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -2,12 +2,20 @@
 def add(x, y):
     return x + y
 
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
 # Predefined numbers
 num1 = 5.0
 num2 = 3.0
 
 # Calculate the sum
-sum = add(num1, num2)
+sum_result = add(num1, num2)
 
-# Display the result
-print("The sum of", num1, "and", num2, "is", sum)
+# Calculate the difference
+difference_result = subtract(num1, num2)
+
+# Display the results
+print("The sum of", num1, "and", num2, "is", sum_result)
+print("The difference between", num1, "and", num2, "is", difference_result)
